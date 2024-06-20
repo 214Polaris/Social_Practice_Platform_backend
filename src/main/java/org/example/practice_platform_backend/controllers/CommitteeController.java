f716437303bbd1721b1b3e02604cf39e252e63dd
Juntao_Chen陈钧韬 <chenjt75@mail2.sysu.edu.cn>
2024-06-20 23:16:04 +0800

    解决issue #1
    加上了一个从token里面取出user_id然后在数据库里做了验证

--- src/main/java/org/example/practice_platform_backend/controllers/CommitteeController.java
+++ src/main/java/org/example/practice_platform_backend/controllers/CommitteeController.java
@@ -27,13 +27,19 @@ public class CommitteeController {
     @Autowired
     private JwtUtils jwtUtils;
 
-
+    // 解析 token 判断是否是校团委
+    private Boolean isValid(HttpServletRequest request){
+        String user_category = jwtUtils.getUserInfoFromToken(request.getHeader("token"), User.class).getUser_category();
+        return Objects.equals(user_category, "committee");
+    }
 
     // 获取所有负责人
     @GetMapping(value="/community/leaders")
     public ResponseEntity<?> getCommunityLeaders(HttpServletRequest request) {
         try {
-
+            if(!isValid(request)){
+                return ResponseEntity.status(400).body("该用户不是校团委");
+            }
             List<CommunityLeader> communityLeaders = communityLeaderService.getCommunityLeaders();
             return ResponseEntity.ok(communityLeaders);
         } catch (Exception e) {
@@ -46,7 +52,9 @@ public class CommitteeController {
     @PostMapping("/modify/community/leader")
     public ResponseEntity<?> deleteCommunityLeader(HttpServletRequest request, @RequestBody Map<String,String> requestBody) {
         try {
-
+            if(!isValid(request)){
+                return ResponseEntity.status(400).body("该用户不是校团委");
+            }
             Map<String, String> result = communityLeaderService.modifyCommunityLeader(requestBody);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
